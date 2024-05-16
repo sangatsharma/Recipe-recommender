18023bd6f366328084c4adc28f97a48382e1af1d
sangatsharma <sangatsharma2@gmail.com>
2024-05-16 20:59:51 +0545

    feat(Homepage): :sparkles: Design components Navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Navbar from './Component/Navbar/Navbar.jsx'
+import Home from './Component/Homepage/Home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <Navbar />
   </React.StrictMode>,
 )
