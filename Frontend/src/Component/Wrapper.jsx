b15f73a875a1fd811b5b789a665cc945b86d8629
sangatsharma <sangatsharma2@gmail.com>
2024-05-21 08:24:26 +0545

    small changes in wrapper components

--- Frontend/src/Component/Wrapper.jsx
+++ Frontend/src/Component/Wrapper.jsx
@@ -1,5 +1,5 @@
 import "./Wrapper.css";
-const Wrapper = ({ children }) => {
-  return <main className="Wrapper">{children}</main>;
+const Wrapper = ({ children,selectedPage }) => {
+  return <main className={`${selectedPage}`}>{children}</main>;
 };
 export default Wrapper; 
