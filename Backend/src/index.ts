a446676bb86ed8012514fbc38b52e0465da4003a
sauravdhakal12 <sauravdhakal121@gmail.com>
2024-06-16 08:58:50 +0545

    Cors implemented

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import recipeRouter from "@/api/recipes/recipes.routes";
 import userRouter from "@/api/users/users.routes";
 
@@ -8,6 +9,8 @@ import { unknownEndPoint } from "@/utils/middleware";
 import { errorHandler } from "@/utils/errorHandler";
 
 const app = express();
+
+app.use(cors({ origin: "https://recipe-recommender-backend.vercel.app" }));
 
 app.use(cookieParser());
 app.use(express.json());
