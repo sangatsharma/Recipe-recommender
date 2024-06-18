a204aadaa6d563739e9794ed2c45220dddf32f86
sangatsharma <sangatsharma2@gmail.com>
2024-06-18 12:29:46 +0545

    typo fix

--- Frontend/src/Pages/HomePage.jsx
+++ Frontend/src/Pages/HomePage.jsx
@@ -2,7 +2,7 @@ import Slider from "../Component/Homepage/Slider/Slider.jsx";
 import TrendingFoods from "../Component/Homepage/TrendingFoodSection/TrendingFoods.jsx";
 import banner1 from '../assets/Images/banner1.jpg';
 import banner2 from '../assets/Images/banner2.jpg';
-import banner3 from '../assets/Images/banner.jpg';
+import banner3 from '../assets/Images/banner3.jpg';
 
 const Homepage = () => {
   const images = [
