0aeb42d38efde7091a954f7f16fc47b19e230073
sauravdhakal12 <sauravdhakal121@gmail.com>
2024-07-04 11:47:13 +0545

    Fix on register

--- Backend/dist/src/api/users/auth/passwordAuth.controllers.js
+++ Backend/dist/src/api/users/auth/passwordAuth.controllers.js
@@ -28,10 +28,13 @@ const userRegisterHandler = async (req, res, next) => {
     };
     try {
         // Register user
-        const b64 = Buffer.from(req.file?.buffer).toString("base64");
-        const dataURI = "data:" + req.file?.mimetype + ";base64," + b64;
-        const cldRes = await (0, cloudinary_1.handleUpload)(dataURI);
-        cleanedBody.profile_pic = cldRes.secure_url;
+        // Create profile picture if provided
+        if (req.file) {
+            const b64 = Buffer.from(req.file?.buffer).toString("base64");
+            const dataURI = "data:" + req.file?.mimetype + ";base64," + b64;
+            const cldRes = await (0, cloudinary_1.handleUpload)(dataURI);
+            cleanedBody.profile_pic = cldRes.secure_url;
+        }
         // TODO: Remove profile picture if user exists
         const userData = await (0, auth_helpers_1.userRegisterHelper)(cleanedBody);
         // Generate token
